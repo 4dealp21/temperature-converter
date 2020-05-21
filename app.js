f1f4c255eab50115acda72b759cc6840e5ab9f06
Pedro de Almeida <56090563+4dealp21@users.noreply.github.com>
2020-05-21 23:08:23 +0100

    Finished temperature-converter app

---
+++ app.js
@@ -0,0 +1,35 @@
+const celsiusInput = document.querySelector('#celsius > input')
+const fahrenheitInput = document.querySelector('#fahrenheit > input')
+const kelvinInput = document.querySelector('#kelvin > input')
+
+function roundNum(num) {
+    return Math.round(num*100)/100;
+}
+
+function CToFandK() {
+    const cTemp = parseFloat(celsiusInput.value);
+    const fTemp = (cTemp * (9/5)) + 32;
+    const kTemp = cTemp + 273.15;
+    fahrenheitInput.value = roundNum(fTemp);
+    kelvinInput.value = roundNum(kTemp);
+}
+
+function FtoCandK() {
+    const fTemp = parseFloat(fahrenheitInput.value);
+    const cTemp = (fTemp - 32) * 5/9;
+    const kTemp = (fTemp + 459.67) * 5/9;
+    celsiusInput.value = roundNum(cTemp);
+    kelvinInput.value = roundNum(kTemp);
+}
+
+function KtoCandF() {
+    const kTemp = parseFloat(kelvinInput.value);
+    const cTemp = kTemp -273.15;
+    const fTemp = 9/5 * (kTemp - 273) + 32;
+    celsiusInput.value = roundNum(cTemp);
+    fahrenheitInput.value = roundNum(fTemp);
+}
+
+celsiusInput.addEventListener('input', CToFandK);
+fahrenheitInput.addEventListener('input', FtoCandK);
+kelvinInput.addEventListener('input', KtoCandF);
